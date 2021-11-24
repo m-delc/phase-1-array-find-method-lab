e71f89500d606e6612bdddcf7c1eca5b729406e1
MichaelDelcambre <delcambrem@gmail.com>
2021-11-24 10:56:47 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,177 @@
+// function superbowlWin(array){
+//     return (result === W)
+// }
 
+
+
+
+// const superbowlWin = array.find(element => result === W)
+// superbowlWin(record)
+
+
+
+
+// function superbowlWin(array){
+//     for (let item of array)
+// }
+
+
+
+
+// function superbowlWin(year){
+
+// }
+
+
+
+// function winResult(record){
+// return (result === 'W')
+// return record.find(winResult)
+// }
+
+
+
+
+// function superbowlWin(record){
+//     for (let record of result)
+//     return 
+// }
+
+
+
+
+
+
+
+// function superbowlWin(record, finder) {
+//     let result = null
+//     for (let item of finder) {
+//         if (finder(finder) === true){
+//             result = finder
+//             break
+//         }
+//     }
+//     return result
+// }
+
+// // will find a result
+
+// function didTheyWin(answer){
+//     return answer.result === `W`
+// }
+
+// superbowlWin(record, didTheyWin)
+
+
+
+
+
+
+
+
+
+
+
+// function superbowlWin(record, finder) {
+//     let result = null
+//     for (let item of result) {
+//         if (finder(result) === true){
+//             result = result
+//             break
+//         }
+//     }
+//     return result
+// }
+
+// // will find a result
+
+// function didTheyWin(answer){
+//     return answer.result === `W`
+// }
+
+// superbowlWin(record, didTheyWin)
+
+
+
+
+
+
+
+// function superbowlWin(record){
+//     for 
+// }
+
+
+
+
+
+
+
+//mahdy's example
+
+// function superbowlWin(record) {
+//     return Array.result === "W"
+    
+// }
+
+// console.log(record.find(superbowlWin).year)
+
+
+
+
+
+// function superbowlWin(record){
+//     return year === 'W' 
+// } 
+
+
+
+
+
+// // this passes test 2
+// function superbowlWin(array){
+//     const result = array.find(x => x.item === "W")
+//     return result
+// }
+
+// function superbowlWin(array){
+//     for(let of )
+// }
+
+
+
+
+
+
+
+
+
+
+
+// //this works, uncomment this
+
+// function superbowlWin (record) {
+//     if (record.find(({result}) => result === 'N/A')){
+//         return superbowlWin = undefined;
+//     }
+// }
+
+// function superbowlWin(record){
+//     if (record.find(({result}) => result === 'W')){
+//         return record.find(({result}) => result === 'W').year
+//     }
+//     }
+
+
+const superbowlWin = (array) => {
+    const obj = array.find((record) => record.result === "W");
+//     if (obj !== undefined {
+//         return obj.year;
+//     } else {
+//         return undefined;
+//     }
+    
+// }
+
+return obj ? obj.year : undefined;
+}
